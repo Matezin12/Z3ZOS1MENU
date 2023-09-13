ae72034375bd7250ef68059648701607358dd1d0
_s4__s4___ <135080071+Matezin12@users.noreply.github.com>
2023-09-13 11:37:34 -0300

    Add files via upload

---
+++ Z3ZOS1M3NU.py
@@ -0,0 +1,120 @@
+import os
+import time
+
+senha = input("Qual a senha? ")
+
+if senha != "989":
+    print("Acesso não liberado")
+    exit()
+else:
+    print("Acesso liberado")
+    time.sleep(3)
+    os.system("clear")
+
+print("Entrando...")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando...")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando...")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando")
+time.sleep(1)
+os.system("clear")
+print("Entrando.")
+time.sleep(1)
+os.system("clear")
+print("Entrando..")
+time.sleep(1)
+os.system("clear")
+print("Entrando...")
+time.sleep(2)
+os.system("clear")
+print("Z3Z 0S1M3NU")
+os.system("clear")
+os.system("git clone https://github.com/Matezin12/Z3z")
+os.system("https://github.com/Matezin12/Z3Z-PUPPET")
+time.sleep(10)
+os.system("clear")
+print("\033[036m ╭━━━━┳━━━┳━━━━╮╭━━━┳━━━╮╭╮╭━╮╭━┳━━━┳━╮╱╭┳╮╱╭╮" )
+print("  ╰━━╮━┃╭━╮┣━━╮━┃┃╭━╮┃╭━╮┣╯┃┃┃╰╯┃┃╭━╮┃┃╰╮┃┃┃╱┃┃ " )
+print("  ╱╱╭╯╭┻╯╭╯┃╱╭╯╭╯┃┃┃┃┃╰━━╋╮┃┃╭╮╭╮┣╯╭╯┃╭╮╰╯┃┃╱┃┃ ")
+print("  ╱╭╯╭╯╭╮╰╮┃╭╯╭╯╱┃┃┃┃┣━━╮┃┃┃┃┃┃┃┃┣╮╰╮┃┃╰╮┃┃╱┃┃")
+print("  ╭╯━╰━┫╰━╯┣╯━╰━╮┃╰━╯┃╰━╯┣╯╰┫┃┃┃┃┃╰━╯┃┃╱┃┃┃╰━╯┃ ")
+print("  ╰━━━━┻━━━┻━━━━╯╰━━━┻━━━┻━━┻╯╰╯╰┻━━━┻╯╱╰━┻━━━╯ \033[036m")
+print("           ")
+print(" \033[1;32m 1: G30L0CK4 SIMPLE v1.0 \033[1;32m   ")
+print(" \033[1;32m 2: P4DR40 LINGU1 Z3Z v1.0 \033[1;32m ")
+print(" \033[1;32m 3: 4CH4 D0M1NIO 1P v1.0 \033[1;32m   ")
+print(" \033[1;32m 4: Z3Z SOCK3T PUPP37 v1.0 \033[1;32m ")
+print(" \033[31m x: sair \033[31m") 
+
+escolha = False
+
+while escolha == False : 
+    nivel = input(" ESC0LH4 4 F3RRAM3NT4: ")
+
+    if nivel == "1":
+        os.system("clear")    
+        os.system ("python3 geo.py") 
+        escolha = True
+
+    elif nivel == "2":
+        os.system("clear")
+        os.system("cd Z3z")
+        os.system("python3 pilt.py")
+        escolha = True
+
+    elif nivel == "3":
+        os.system("clear")
+        os.system("cd Z3z")
+        os.system("python3 s7.py")
+        escolha = True
+
+    elif nivel == "4":
+        os.system("clear")
+        os.system("python3 Z3Z-PUPPET")
+        escolha = True
+
+    elif nivel == "x":
+        os.system("clear")    
+        os.system ("cd ~ ")
+        escolha = True
