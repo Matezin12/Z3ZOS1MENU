4f3313d312b46adb96955fa13f0a35da38a61f11
_s4__s4___ <135080071+Matezin12@users.noreply.github.com>
2023-09-13 22:33:02 -0300

    Update omainst.py

--- omainst.py
+++ omainst.py
@@ -106,7 +106,7 @@ while not escolha:
 
     elif nivel == "4":
         os.system("clear")
-        os.system("cd oshdr")
+        os.system("cd .. && cd oshdr")
         os.system("python3 os.py")
         escolha = True
 
