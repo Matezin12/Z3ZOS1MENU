af91fdbd7e2b341d8ab605f8aff1a81c487c20d0
_s4__s4___ <135080071+Matezin12@users.noreply.github.com>
2023-10-03 21:55:56 -0300

    Add files via upload

---
+++ niam.py
@@ -0,0 +1,43 @@
+time.sleep(10)
+os.system("clear")
+print(" \033[036m ▒█▀▀▀█ ▒█▀▀▀█ ▄█░ ▒█▀▄▀█ █▀▀█ ")
+print("  ▒█░░▒█ ░▀▀▀▄▄ ░█░ ▒█▒█▒█ ░░▀▄ ")
+print("  ▒█▄▄▄█ ▒█▄▄▄█ ▄█▄ ▒█░░▒█ █▄▄█ \033[036m ")
+print("                                  ")
+print(" \033[1;32m 1: G30L0CK4 SIMPLE v1.0 \033[1;32m   ")
+print(" \033[1;32m 2: P4DR40 LINGU1 Z3Z v1.0 \033[1;32m ")
+print(" \033[1;32m 3: 4CH4 D0M1NIO 1P v1.0 \033[1;32m   ")
+print(" \033[1;32m 4: Z3Z SOCK3T PUPP37 v1.0 \033[1;32m ")
+print(" \033[31m x: sair \033[31m") 
+
+escolha = False
+
+while not escolha: 
+    nivel = input(" ESC0LH4 4 F3RRAM3NT4: ")
+
+    if nivel == "1":
+        os.system("clear") 
+        os.system("cd srd && python3 trc.py") 
+        escolha = True
+
+    elif nivel == "2":
+        os.system("clear")
+        os.system("cd srd && python3 hrd.py")
+        escolha = True
+
+    elif nivel == "3":
+        os.system("clear")
+        os.system("cd srd && python3 fsh.py")
+        escolha = True
+
+    elif nivel == "4":
+        os.system("clear")
+        os.chdir("oshdr")         
+        os.system("python3 os.py")
+        escolha = True
+
+    elif nivel == "x":
+        os.system("clear")    
+        os.chdir("~")
+        escolha = True
+    
